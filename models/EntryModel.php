d03d946f1d551c69f1bc5cf04d9d3f4a98a28866
godlovesdavid <godlovesdavid@gmail.com>
2013-10-28 13:22:03 -0700

    added more comments, change formview

--- models/EntryModel.php
+++ models/EntryModel.php
@@ -1,69 +1,219 @@
 <?php
 class EntryModel
 {
+	//gets data from arguments.
 	static function getData($id)
 	{
-		global $authenticate;
-		$connection = mysqli_connect("localhost", $authenticate['user'], 
-			$authenticate['pass'], "poems");
-		if (isset($id['check']))
-		{
-			$data = self::checkForm();
-			return $data;
-		}
-		else if (isset($id['form']))
-			return '';
-		else if ($id == 'featured')
-			$data['requestedPoems'][] = mysqli_fetch_assoc(
-				mysqli_query($connection, "SELECT * FROM featured"));
-		else if ($id == 'random')
-			$data['requestedPoems'][] = mysqli_fetch_assoc(
-				mysqli_query($connection, "SELECT * FROM random"));
-		else if ($id == 'top10')
-			while ($row = mysqli_fetch_assoc(
-				mysqli_query($connection, "SELECT * FROM top10")))
-				$data['requestedPoems'][] = $row;
-		else if ($id == 'newest')
-			$data['requestedPoems'][] = mysqli_fetch_assoc(
-				mysqli_query($connection, "SELECT * FROM newest"));
-		else if ($id == '')
-			$data['requestedPoems'][] = mysqli_fetch_assoc(
-				mysqli_query($connection, "SELECT * FROM featured"));
-		else
-			$data['requestedPoems'][] = mysqli_fetch_assoc(
-				mysqli_query($connection, "SELECT * FROM all WHERE id=" . $id));
+		if ($id == 'rate')
+			$data = self::rate();
+		
+		if ($id == 'checkform')
+			$data['checkresult'] = self::checkFormAndSubmitPoem();
+		if ($id == 'none')
+			$data['checkresult'] = '';
+		
+		if (is_numeric($id))
+			$data = self::getEntry($id);
+		if ($id == '' || $id == 'featured')
+			$data = self::getFeatured();
+		if ($id == 'random')
+			$data = self::getRandom();
+		if ($id == 'top10')
+			$data = self::getTop10();
+		if ($id == 'newest10')
+			$data = self::getNewest10();
+		if ($id == 'newest')
+			$data = self::getNewest();
+		$data['links'] = self::getLinks();
 		return $data;
 	}
-	static function checkForm()
+	
+	//connect to database.
+	static function connectToDB()
+	{
+		global $authenticate;
+		$connection = mysqli_connect($authenticate['host'], 
+			$authenticate['user'], $authenticate['pass'], 
+			$authenticate['dbname']);
+		return $connection;
+	}
+	
+	// append star info so that view can draw.
+	static function appendRating($poem)
+	{
+		if (! isset($poem['rating']))
+		{
+			if ($poem['votes'] == 0)
+				$poem['rating'] = 0;
+			else
+				$poem['rating'] = $poem['value'] / $poem['votes'];
+		}
+		$poem['stars'] = '';
+		for ($i = 1; $i <= 5; $i ++) // calculate what a star looks like
+		{
+			if ($poem['rating'] >= $i) // 1 full star
+				$class = "star_" . $i . " norating_stars rating_voted";
+			else
+			{
+				if ($poem['rating'] > $i - 1 + .74) // close enough, give it the
+				                                    // full star
+					$class = "star_" . $i . " norating_stars rating_voted";
+				else if ($poem['rating'] > $i - 1 + .24 &&
+					 $poem['rating'] < $i - 1 + .75) // between
+					                                // .25
+					                                // and
+					                                // .74,
+					                                // inclusive
+					$class = "star_" . $i . " norating_stars rating_halfvoted";
+				else
+					$class = "star_" . $i . " norating_stars rating_blank";
+			}
+			$poem['stars'] .= '<div class="' . $class . '"></div>';
+		}
+		return $poem;
+	}
+	
+	//rate action call that saves a user rating.
+	static function rate()
+	{
+		header("Pragma: nocache");
+		header("Cache-Control: no-cache");
+		$id_sent = preg_replace("/[^0-9]/", "", $_REQUEST['id']);
+		$vote_sent = preg_replace("/[^0-9]/", "", $_REQUEST['stars']);
+		$connection = self::connectToDB();
+		$numbers = mysqli_fetch_assoc(
+			mysqli_query($connection, 
+				"select votes, value from poems where id=" . $id_sent));
+		$sum = $vote_sent + $numbers['value'];
+		($sum == 0? $added = 0 : $added = $numbers['votes'] + 1);
+		if ($vote_sent <= 5 && $vote_sent >= 1)
+			mysqli_query($connection, 
+				"update poems set votes='" . $added . "', value='" . $sum .
+					 "' where id=" . $id_sent);
+		$data = mysqli_fetch_assoc(
+			mysqli_query($connection, 
+				"select votes, value, id from poems where id=" . $id_sent));
+		$data['rated'] = 'true';
+		return self::appendRating($data);
+	}
+	
+	//get list of poem entry links in html.
+	static function getLinks()
+	{
+		$result = mysqli_query(self::connectToDB(), 
+			"select * from poems order by timestamp");
+		$links = '';
+		while ($row = mysqli_fetch_assoc($result))
+			$links .= '<a href="index.php?main&e=' . $row['id'] . '">' .
+				 $row['title'] . ' by ' . $row['author'] . '</a><br/>';
+		return $links;
+	}
+	
+	//get featured poem.
+	static function getFeatured()
+	{
+		$data['header'] = 'Featured Poem';
+		$data['requestedPoems'][] = self::appendRating(
+			mysqli_fetch_assoc(
+				mysqli_query(self::connectToDB(), 
+					"select * from poems where featured=1")));
+		return $data;
+	}
+	
+	//get random poem.
+	static function getRandom()
+	{
+		$data['header'] = 'Random Poem';
+		$data['requestedPoems'][] = self::appendRating(
+			mysqli_fetch_assoc(
+				mysqli_query(self::connectToDB(), 
+					"select * from poems order by rand()")));
+		return $data;
+	}
+	
+	//get top 10 poems.
+	static function getTop10()
+	{
+		$data['header'] = 'Top 10 Poems';
+		$result = mysqli_query(self::connectToDB(), 
+			"select * from poems order by (value / votes) desc limit 10");
+		while ($row = mysqli_fetch_assoc($result))
+			$data['requestedPoems'][] = self::appendRating($row);
+		return $data;
+	}
+	
+	//get newest poem.
+	static function getNewest()
+	{
+		$data['header'] = 'Newest Poem';
+		$data['requestedPoems'][] = self::appendRating(
+			mysqli_fetch_assoc(
+				mysqli_query(self::connectToDB(), 
+					"select * from poems order by timestamp desc")));
+		return $data;
+	}
+	
+	//get newest 10 poems.
+	static function getNewest10()
+	{
+		$data['header'] = 'Newest 10 Poems';
+		$result = mysqli_query(self::connectToDB(), 
+			"select * from poems order by timestamp desc limit 10");
+		while ($row = mysqli_fetch_assoc($result))
+			$data['requestedPoems'][] = self::appendRating($row);
+		return $data;
+	}
+	
+	//get requested poem.
+	static function getEntry($id)
+	{
+		$data['header'] = '';
+		$data['requestedPoems'][] = self::appendRating(
+			mysqli_fetch_assoc(
+				mysqli_query(self::connectToDB(), 
+					"select * from poems where id=" . $id)));
+		return $data;
+	}
+	
+	//check form after submission, and submit it.
+	static function checkFormAndSubmitPoem()
 	{
 		list($a1, $a2, $b1, $b2, $a3) = explode("\n", $_POST['content']);
 		if (! EntryModel::doesRhyme($a1, $a2) ||
 			 ! EntryModel::doesRhyme($a1, $a3) || ! EntryModel::doesRhyme($b1, 
 				$b2))
-			$result = "Rhyming Scheme error";
-		else
+			return "Rhyming Scheme error";
+		else // enter data
 		{
 			if (self::enterData($_POST))
-				$result = "successfully added";
+				return "Successfully added poem";
 			else
-				$result = "insert into database error";
+				return "Insert into database error";
 		}
-		return $result;
 	}
+	
+	//append real escape string to poem and adds it to db.
 	static function enterData($data)
 	{
+		$id = null; // mysql will auto increment
 		global $authenticate;
-		$id = time(); // use unix timestamp for id
-		$title = addslashes($data['title']);
-		$author = addslashes($data['author']);
-		$date = date("m\.d\.y");
-		$content = addslashes($data['content']);
-		$rating = 0; //initial rating
-		return mysqli_query(
-			mysqli_connect("localhost", $authenticate['user'], 
-				$authenticate['pass'], "poems"), 
-			"INSERT INTO total VALUES ('$id', '$title', '$author', '$date', '$content', '$rating')");
+		$connection = self::connectToDB();
+		// must add slashes
+		$title = mysqli_real_escape_string($connection, $data['title']);
+		$author = mysqli_real_escape_string($connection, $data['author']);
+		$timestamp = time();
+		$content = mysqli_real_escape_string($connection, $data['content']);
+		$value = 0.00; // initial value
+		
+		if (mysqli_query($connection, 
+			"INSERT INTO poems VALUES ('$id', '$title', '$author', '$timestamp', '$content', 0, 0, '$value')"))
+			return true;
+		else
+			print mysqli_error($connection);
+		return false;
 	}
+	
+	//check if 2 lines rhyme.
 	static function doesRhyme($line1, $line2)
 	{
 		$words1 = preg_split('/\s+/', 
--- models/Model.php
+++ models/Model.php
@@ -1,6 +1,8 @@
 <?php
+//abstract class model.
 abstract class Model
 {
+	//gets data from database or wherever.
 	abstract static function getData($name);
 }
 
