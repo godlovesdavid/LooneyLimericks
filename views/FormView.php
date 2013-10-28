83e55ffed86a1ff31510af7fc2c7a13b44fe5584
godlovesdavid <godlovesdavid@gmail.com>
2013-10-28 13:03:05 -0700

    added comments

--- views/FormView.php
+++ views/FormView.php
@@ -1,6 +1,10 @@
 <?php
-class FormView
+include 'views/View.php';
+
+//class form view that handles poem submission form.
+class FormView extends View
 {
+	//display data in html.
 	static function display($data)
 	{
 		include 'FormView.html';
--- views/MainView.php
+++ views/MainView.php
@@ -1,6 +1,11 @@
 <?php
-class MainView
+include 'views/View.php';
+
+//main view that shows poems.
+class MainView extends View
 {
+	
+	//use html to show data passed by controller.
 	static function display($data)
 	{
 		include 'MainView.html';
